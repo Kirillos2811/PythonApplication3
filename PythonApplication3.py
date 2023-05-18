6233ff7590b47140bc7cb95492145ef54e4d7f93
Kirillos2811 <kirill.sesorov@gmail.com>
2023-05-18 23:02:03 +0300

    Lesson 5 task 3

--- PythonApplication3.py
+++ PythonApplication3.py
@@ -1,1 +1,14 @@
+a = float(input("a = "))
+b = float(input("b = "))
+x = float(input("x = "))
 
+if a >= x and b >= x:
+    print(2)
+elif a >= x:
+    print("Mike")
+elif b >= x:
+    print("Ivan")
+elif a + b >= x:
+    print(1)
+else:
+    print(0)
